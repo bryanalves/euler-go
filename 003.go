01a2a006c6b75e922bca4d354ec2b97ecf862607
Bryan Alves <bryanalves@gmail.com>
2017-03-28 23:27:56 -0400

    Euler 3

---
+++ 003.go
@@ -0,0 +1,17 @@
+// https://projecteuler.net/problem=3
+
+package main
+
+func problem_3(composite int) int {
+    factor := 2
+
+    for composite > 2 {
+        if (composite % factor) == 0 {
+            composite /= factor
+        } else {
+            factor++
+        }
+    }
+
+    return factor
+}
---
+++ 003_test.go
@@ -0,0 +1,12 @@
+package main
+
+import "testing"
+
+func TestProblem3(t *testing.T) {
+    out := problem_3(600851475143)
+    expected := 6857
+
+    if out != expected {
+      t.Errorf("problem_3(600851475143) = %d, should be %d", out, expected)
+  }
+}
