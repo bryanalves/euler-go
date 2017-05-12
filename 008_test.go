33d95e9992457e4c303f9349d6e047878cdd5dfe
Bryan Alves <bryanalves@gmail.com>
2017-05-11 22:52:57 -0400

    Euler 8

---
+++ 008_test.go
@@ -0,0 +1,12 @@
+package main
+
+import "testing"
+
+func TestProblem8(t *testing.T) {
+    out := problem_8()
+    expected := 23514624000
+
+    if out != expected {
+        t.Errorf("problem_8() = %d, should be %d", out, expected)
+    }
+}
